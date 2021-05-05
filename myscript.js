436597871e1f3b5b08f7ae38a5fbf96b722d9543
MVG-Innovations <83455446+mvg-innovations@users.noreply.github.com>
2021-05-05 16:57:39 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,5 @@
+function myfunction() {
+                  var text1="UG";
+                  var text2="PG";
+                  var text3=text1 + text2
+              document.getElementById("demo").innerHTML=text3
